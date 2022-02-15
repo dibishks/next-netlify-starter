148bb64a1d586c7094a3c78481627a184f9e9027
DIBEESH KS <dibishks_607@yahoo.co.in>
2022-02-15 12:38:45 +0530

    Update index.js
    removed form

--- pages/index.js
+++ pages/index.js
@@ -16,27 +16,6 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
     
-    <!-- A little help for the Netlify post-processing bots -->
- <form name="contact" netlify netlify-honeypot="bot-field" hidden>
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <textarea name="message"></textarea>
-    </form>
-    <form name="contact" method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Your Name: <input type="text" name="name"/></label>
-          </p>
-          <p>
-            <label>Your Email: <input type="email" name="email"/></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
 
       </main>
 
