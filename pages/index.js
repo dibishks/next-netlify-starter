6bc29b9631a53355456c531c2e40f7757fd89a16
DIBEESH KS <dibishks_607@yahoo.co.in>
2022-02-15 12:35:50 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,29 +15,29 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
-<form name="simpleContactForm" method="POST" data-netlify="true" id="simple-contact-form" class="contact-form">
-    <p class="form-row">
-        <label id="contact-form-name-label" for="contact-form-name" class="form-label">Name</label>
-        <input type="text" name="name" id="contact-form-name" aria-labelledby="contact-form-name-label" class="form-input" />
-    </p>
-    <p class="form-row">
-        <label id="contact-form-email-label" for="contact-form-email" class="form-label">Email address</label>
-        <input type="email" name="email" id="contact-form-email" aria-labelledby="contact-form-email-label" class="form-input" />
-    </p>
-    <p class="form-row">
-        <label id="contact-form-message-label" for="contact-form-message" class="form-label">Message</label>
-        <textarea
-            name="message"
-            id="contact-form-message"
-            aria-labelledby="contact-form-message-label"
-            class="form-textarea"
-            rows="7"
-        ></textarea>
-    </p>
-    <p class="form-row form-submit">
-        <button type="submit" class="button">Send Message</button>
-    </p>
-</form>
+    
+    <!-- A little help for the Netlify post-processing bots -->
+ <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message"></textarea>
+    </form>
+    <form name="contact" method="post">
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+
       </main>
 
       <Footer />
